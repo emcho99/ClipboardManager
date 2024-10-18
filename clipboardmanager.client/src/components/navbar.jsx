a1cacd73345aa9d88f2e29c3f17c93a969803371
almink9 <akrbezlija@gmail.com>
2024-10-18 22:03:22 +0200

    Added gitignore file.

--- clipboardmanager.client/src/components/navbar.jsx
+++ clipboardmanager.client/src/components/navbar.jsx
@@ -1,32 +1,33 @@
-import React from "react";
-import "./Navbar.css";
+import React from 'react';
+import './Navbar.css';
 
 const Navbar = () => {
   return (
-    <nav className="navbar">
-      <div className="navbar-right">
-        <a href="#">Clipboard Manager</a>
-      </div>
 
-      <div className="navbar-center">
-        <ul className="navbar-links">
-          <li>
-            <a href="#">Clipboard</a>
-          </li>
-          <li>
-            <a href="#">Friends</a>
-          </li>
-          <li>
-            <a href="#">Top Clips</a>
-          </li>
+<nav className="navbar">
+    <div classname="navbar-right">
+        <a>Clipboard Manager</a>
+    </div>
+
+    <div classname="navbar-center">
+        <ul classname="navbar-links">
+            <li>
+                <a>Clipboard</a>
+              </li>
+            <li>
+                <a>Friends</a>
+              </li>
+            <li>
+                <a>Top Clips</a>
+              </li>
         </ul>
-      </div>
+    </div>
 
-      <div className="navbar-left">
-        <a href="#">Settings</a>
-      </div>
-    </nav>
-  );
+    <div classname="navbar-left">
+        <a>Settings</a>
+    </div>
+</nav>
+);
 };
 
 export default Navbar;
